3df2180adc2c0562c8d69ec0189950cd7b9d1365
yuanjianye <yuanjianye@126.com>
2015-04-29 16:02:24 +0800

    add ddns.sh
    Signed-off-by: yuanjianye <yuanjianye@126.com>

---
+++ ddns/ddns.sh
@@ -0,0 +1,24 @@
+cd /tmp/
+mkdir myddns
+cd myddns
+wget http://182.254.161.98/ddns -o /dev/null 
+if [ "$?" == "0" ];
+then
+     MY_IP=`cat ddns|awk '{print $1}'`
+     HOME_IP=`cat ddns|awk '{print $2}'`
+     if [ "$MY_IP" != "" ] && [ "$HOME_IP" != "" ];
+     then
+         #echo "my ip is $MY_IP"
+         #echo "home ip is $HOME_IP"
+         if [ "$MY_IP" != "$HOME_IP" ];
+         then
+             echo "do renew script"
+             ssh www.yuanjianye.com "renew_ddns.sh $MY_IP"
+         fi
+     else
+         echo "file unexpect" >&2
+     fi
+else 
+    echo "wget error" >&2
+fi
+rm -r /tmp/myddns
