28cb59b10fd425f680d6ce2090bc410cc7afde78
yuanjianye <yuanjianye@126.com>
2015-05-10 22:33:55 +0800

    add vundle
    Change-Id: I98955d6058769d8b2029cfeaaa50b602413efb79
    Signed-off-by: yuanjianye <yuanjianye@126.com>

--- apt-get/install.sh
+++ apt-get/install.sh
@@ -1,1 +1,4 @@
-apt-get install vim tmux fbterm git subversion python bison mysql-server apache2 php5
+apt-get install vim tmux fbterm 
+apt-get install git subversion git-svn
+apt-get install python mysql-server apache2 php5 
+apt-get install libc6-i386 flex zip libxml2-utils gperf git gnupg flex bison gperf build-essential   zip curl libc6-dev x11proto-core-dev   g++-multilib  tofrodos   python-markdown libxml2-utils xsltproc bison curl
